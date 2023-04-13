cbf06bfe69d69d1d541403389e219c0b465c45ec
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-04-13 16:43:24 +1000

    Create type crypto.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
-import { log } from "console";
+
+export type Crypto = {
+  ath: number;
+  atl: number;
+  current_price: number;
+  fully_diluted_valuation: 942450877195;
+  high_24h: number;
+  id: string;
+  low_24h: number;
+  name: string;
+  symbol: string;
+};
 
 function App() {
-  const [cryptos, setCryptos] = useState([]);
+  const [cryptos, setCryptos] = useState<Crypto[] | null>();
   useEffect(() => {
     const url =
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&order=market_cap_desc&per_page=10&page=1&sparkline=false";
@@ -13,9 +24,17 @@ function App() {
       setCryptos(response.data);
     });
   }, []);
-  console.log(cryptos);
+  // console.log(cryptos);
 
-  return <div className='App'>{cryptos ? cryptos : null}</div>;
+  return (
+    <div className='App'>
+      {cryptos
+        ? cryptos.map((crypto) => {
+            return <p>{`${crypto.name}  $${crypto.current_price}`}</p>;
+          })
+        : null}
+    </div>
+  );
 }
 
 export default App;
